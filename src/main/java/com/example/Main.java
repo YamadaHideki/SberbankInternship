b88d46ef9afd5fd6c7d83abf330db91b97f300e9
YamadaHideki <top.secret.work@yandex.ru>
2022-06-27 17:01:06 +0300

    add sort realization

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,8 @@ package com.example;
 import com.example.model.City;
 import com.example.repository.CitiesRepository;
 import java.io.File;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -21,7 +23,23 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (City c : citiesRepository.getCityList()) {
+        List<City> citiesSortByName = citiesRepository.getCityList();
+        citiesSortByName.sort(
+                Comparator.comparing(a -> a.getName().toLowerCase())
+        );
+
+        List<City> citiesSortByDistrictAndName = citiesRepository.getCityList();
+        citiesSortByDistrictAndName.sort(
+                Comparator.comparing(City::getDistrict).thenComparing(City::getName)
+        );
+
+        System.out.println("Пример полученного результата для сортировки по наименованию:");
+        for (City c : citiesSortByName) {
+            System.out.println(c);
+        }
+
+        System.out.println("Пример полученного результата для сортировки по двум полям справочника – федеральному округу и наименованию города:");
+        for (City c : citiesSortByDistrictAndName) {
             System.out.println(c);
         }
     }
